8f392312568a27abee2cf12708692533687e3c72
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-03 13:00:37 +0000

    Add some routes and detail

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, url_for, jsonify
 from urllib.parse import quote
-from Controller import MahasiswaController, PerguruanTinggiController
+from Controller import MahasiswaController, PerguruanTinggiController, ProdiController, HitController, DosenController
 
 app = Flask(__name__)
 
@@ -10,11 +10,11 @@ def home():
     'owner': [
       {
       'name'   : 'Ridwan Halim',
-      'address': 'Madinah, Saudi Arabia',
+      'address': 'Boyolali, Central Java',
       'my_wife': 'Hafidhah Afkariana'
       }
     ],
-    'social media': [
+    'social_media': [
       {
       'instagram': 'https://www.instagram.com/ridwaanhall',
       'facebook' : 'https://www.facebook.com/ridwaanhall',
@@ -25,6 +25,22 @@ def home():
       'github'   : 'https://github.com/ridwaanhall',
       'replit'   : 'https://replit.com/@ridwaanhall',
       'telegram' : 'https://t.me/ridwaanhall'
+      }
+    ],
+    'routes_available': [
+      {
+        'url_base' : 'https://data-mahasiswa.ridwaanhall.repl.co',
+        'route': '/hit_mhs',
+        'note': 'this route for search mahasiswa data. such as name, etc.'
+      },
+      {
+        
+      },
+      {
+        
+      },
+      {
+        
       }
     ]
   }
@@ -153,12 +169,9 @@ def data_pt_jumlah_detail(link_pt):
 @app.route('/data_pt_dosen', methods=['GET', 'POST'])
 def data_pt_dosen():
   if request.method == 'POST':
-    # Get the input from the form
     link_pt = request.form['link_pt']
-    # Redirect to the route with the input as a parameter
     return redirect(url_for('data_pt_dosen_detail', link_pt=link_pt))
   else:
-    # Display the input form
     return '''
     <p>input ID perguruan tinggi / id_sp pada load_pt</p>
     <form method="post" action="/data_pt_dosen">
@@ -170,8 +183,75 @@ def data_pt_dosen():
 
 @app.route('/data_pt_dosen/<string:link_pt>', methods=['GET'])
 def data_pt_dosen_detail(link_pt):
-    data = PerguruanTinggiController.data_pt_dosen(link_pt)
-    return jsonify(data)
-  
+  data = PerguruanTinggiController.data_pt_dosen(link_pt)
+  return jsonify(data)
+
+@app.route('/stat_pt', methods=['GET', 'POST'])
+def stat_pt():
+  if request.method == 'POST':
+    link_pt = request.form['link_pt']
+    return redirect(url_for('stat_pt_detail', link_pt=link_pt))
+  else:
+    return '''
+    <p>input ID perguruan tinggi / id_sp pada load_pt</p>
+    <form method="post" action="/stat_pt">
+      <label for="link_pt">ID:</label>
+      <input type="text" id="link_pt" name="link_pt" required>
+      <input type="submit" value="OK">
+    </form>
+    '''
+
+@app.route('/stat_pt/<string:link_pt>', methods=['GET'])
+def stat_pt_detail(link_pt):
+  data = PerguruanTinggiController.stat_pt(link_pt)
+  return jsonify(data)
+
+@app.route('/load_prodi', methods=['GET'])
+def load_prodi():
+  data = ProdiController.load_prodi()
+  return jsonify(data)
+
+# search prodi using id_sp (pt)
+@app.route('/load_prodi/<string:id_sp>', methods=['GET'])
+def load_detail_prodi(id_sp):
+  data = ProdiController.load_detail_prodi(id_sp)
+  return jsonify(data)
+
+@app.route('/data_prodi/<string:link_prodi>', methods=['GET'])
+def data_prodi(link_prodi):
+  data = ProdiController.data_prodi(link_prodi)
+  return jsonify(data)
+
+@app.route('/data_dosen/<string:link_dosen>', methods=['GET'])
+def data_dosen(link_dosen):
+  data = DosenController.data_dosen(link_dosen)
+  return jsonify(data)
+
+@app.route('/hit', methods=['GET', 'POST'])
+def hit():
+  if request.method == 'POST':
+    # Get the input from the form
+    dsn_prodi_pt = request.form['dsn_prodi_pt']
+    # Redirect to the route with the input as a parameter
+    return redirect(url_for('hit_dsn_prodi_pt_detail', dsn_prodi_pt=dsn_prodi_pt))
+  else:
+    # Display the input form
+    return '''
+    <p>dosen, pt, prodi.</p>
+    <form method="post" action="/hit">
+      <label for="dsn_prodi_pt">dosen pt prodi:</label>
+      <input type="text" id="dsn_prodi_pt" name="dsn_prodi_pt" required>
+      <input type="submit" value="OK">
+    </form>
+    '''
+
+@app.route('/hit/<string:dsn_prodi_pt>', methods=['GET'])
+def hit_dsn_prodi_pt_detail(dsn_prodi_pt):
+  # Encode the dsn_prodi_pt parameter to handle special characters
+  encoded_dsn_prodi_pt = quote(dsn_prodi_pt)
+  # Use the encoded_dsn_prodi_pt as input for the dsn_prodi_ptController.hit_mhs() function
+  data = HitController.hit(encoded_dsn_prodi_pt)
+  return jsonify(data)
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)

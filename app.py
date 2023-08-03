63cdcb1af26890b513cb4beea8c243846c0a6940
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-03 11:33:38 +0000

    Add data in route base, add routes such as data_pt_prodi, data_pt_jumlah, data_pt_dosen also detail for routes.

--- app.py
+++ app.py
@@ -5,8 +5,29 @@ from Controller import MahasiswaController, PerguruanTinggiController
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-  return "url : ridwaanhall"
+def home():
+  return {
+    'owner': [
+      {
+      'name'   : 'Ridwan Halim',
+      'address': 'Madinah, Saudi Arabia',
+      'my_wife': 'Hafidhah Afkariana'
+      }
+    ],
+    'social media': [
+      {
+      'instagram': 'https://www.instagram.com/ridwaanhall',
+      'facebook' : 'https://www.facebook.com/ridwaanhall',
+      'tiktok'   : 'https://www.tiktok.com/@ridwaanhall',
+      'twitter'  : 'https://twitter.com/ridwaanhall',
+      'threads'  : 'https://www.threads.net/@ridwaanhall',
+      'linkedin' : 'https://www.linkedin.com/in/ridwaanhall',
+      'github'   : 'https://github.com/ridwaanhall',
+      'replit'   : 'https://replit.com/@ridwaanhall',
+      'telegram' : 'https://t.me/ridwaanhall'
+      }
+    ]
+  }
 
 @app.route('/hit_mhs', methods=['GET', 'POST'])
 def hit_mhs():
@@ -14,11 +35,11 @@ def hit_mhs():
     # Get the input from the form
     mahasiswa = request.form['mahasiswa']
     # Redirect to the route with the input as a parameter
-    return redirect(url_for('hit_mhs_mhs', mahasiswa=mahasiswa))
+    return redirect(url_for('hit_mhs_detail', mahasiswa=mahasiswa))
   else:
     # Display the input form
     return '''
-    <h2>bisa input NIM, nama, nama + kampus, dll.</h2>
+    <p>bisa input NIM, nama, nama + kampus, dll.</p>
     <form method="post" action="/hit_mhs">
       <label for="mahasiswa">Mahasiswa:</label>
       <input type="text" id="mahasiswa" name="mahasiswa" required>
@@ -27,10 +48,8 @@ def hit_mhs():
     '''
 
 @app.route('/hit_mhs/<string:mahasiswa>', methods=['GET'])
-def hit_mhs_mhs(mahasiswa):
-  # Encode the mahasiswa parameter to handle special characters
+def hit_mhs_detail(mahasiswa):
   encoded_mahasiswa = quote(mahasiswa)
-  # Use the encoded_mahasiswa as input for the MahasiswaController.hit_mhs() function
   data = MahasiswaController.hit_mhs(encoded_mahasiswa)
   return jsonify(data)
 
@@ -44,7 +63,7 @@ def data_mahasiswa_home():
   else:
     # Display the input form
     return '''
-    <h2>input id mahasiswa</h2>
+    <p>input id mahasiswa</p>
     <form method="post" action="/data_mahasiswa">
       <label for="mahasiswa">id mahasiswa:</label>
       <input type="text" id="id_mahasiswa" name="id_mahasiswa" required>
@@ -62,10 +81,97 @@ def load_pt():
   data = PerguruanTinggiController.load_pt()
   return jsonify(data)
 
+@app.route('/data_pt', methods=['GET', 'POST'])
+def data_pt():
+  if request.method == 'POST':
+    # Get the input from the form
+    link_pt = request.form['link_pt']
+    # Redirect to the route with the input as a parameter
+    return redirect(url_for('data_pt_detail', link_pt=link_pt))
+  else:
+    # Display the input form
+    return '''
+    <p>input ID perguruan tinggi / id_sp pada load_pt</p>
+    <form method="post" action="/data_pt">
+      <label for="link_pt">Mahasiswa:</label>
+      <input type="text" id="link_pt" name="link_pt" required>
+      <input type="submit" value="OK">
+    </form>
+    '''
+
 @app.route('/data_pt/<string:link_pt>', methods=['GET'])
-def data_pt(link_pt):
+def data_pt_detail(link_pt):
   data = PerguruanTinggiController.data_pt(link_pt)
   return jsonify(data)
 
+@app.route('/data_pt_prodi', methods=['GET', 'POST'])
+def data_pt_prodi():
+  if request.method == 'POST':
+    # Get the input from the form
+    link_pt = request.form['link_pt']
+    # Redirect to the route with the input as a parameter
+    return redirect(url_for('data_pt_prodi_detail', link_pt=link_pt))
+  else:
+    # Display the input form
+    return '''
+    <p>input ID perguruan tinggi / id_sp pada load_pt</p>
+    <form method="post" action="/data_pt_prodi">
+      <label for="link_pt">ID:</label>
+      <input type="text" id="link_pt" name="link_pt" required>
+      <input type="submit" value="OK">
+    </form>
+    '''
+
+@app.route('/data_pt_prodi/<string:link_pt>', methods=['GET'])
+def data_pt_prodi_detail(link_pt):
+    data = PerguruanTinggiController.data_pt_prodi(link_pt)
+    return jsonify(data)
+
+@app.route('/data_pt_jumlah', methods=['GET', 'POST'])
+def data_pt_jumlah():
+  if request.method == 'POST':
+    # Get the input from the form
+    link_pt = request.form['link_pt']
+    # Redirect to the route with the input as a parameter
+    return redirect(url_for('data_pt_jumlah_detail', link_pt=link_pt))
+  else:
+    # Display the input form
+    return '''
+    <p>input ID perguruan tinggi / id_sp pada load_pt</p>
+    <form method="post" action="/data_pt_jumlah">
+      <label for="link_pt">ID:</label>
+      <input type="text" id="link_pt" name="link_pt" required>
+      <input type="submit" value="OK">
+    </form>
+    '''
+
+@app.route('/data_pt_jumlah/<string:link_pt>', methods=['GET'])
+def data_pt_jumlah_detail(link_pt):
+    data = PerguruanTinggiController.data_pt_jumlah(link_pt)
+    return jsonify(data)
+
+@app.route('/data_pt_dosen', methods=['GET', 'POST'])
+def data_pt_dosen():
+  if request.method == 'POST':
+    # Get the input from the form
+    link_pt = request.form['link_pt']
+    # Redirect to the route with the input as a parameter
+    return redirect(url_for('data_pt_dosen_detail', link_pt=link_pt))
+  else:
+    # Display the input form
+    return '''
+    <p>input ID perguruan tinggi / id_sp pada load_pt</p>
+    <form method="post" action="/data_pt_dosen">
+      <label for="link_pt">ID:</label>
+      <input type="text" id="link_pt" name="link_pt" required>
+      <input type="submit" value="OK">
+    </form>
+    '''
+
+@app.route('/data_pt_dosen/<string:link_pt>', methods=['GET'])
+def data_pt_dosen_detail(link_pt):
+    data = PerguruanTinggiController.data_pt_dosen(link_pt)
+    return jsonify(data)
+  
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)

573bfce691ab3b5e6df404ed66cf1cc1ae8374c8
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-04 12:35:28 +0000

    add how_to_use, modify note to z_note.

--- app.py
+++ app.py
@@ -8,10 +8,13 @@ app = Flask(__name__)
 @app.route("/")
 def home():
   return {
+    'how_to_use': [{
+      'text': 'you can use this API from link in routes_available.'
+    }],
     'owner': [{
       'name': 'ridwaanhall',
       'address': 'Sleman, DIY',
-      'my_wife': 'Afida'
+      'my_love': 'Afida'
     }],
     'social_media': [{
       'instagram': 'https://www.instagram.com/ridwaanhall',
@@ -24,178 +27,156 @@ def home():
       'replit': 'https://replit.com/@ridwaanhall',
       'telegram': 'https://t.me/ridwaanhall'
     }],
-    'routes_available': 
-    [
-      {
+    'routes_available': [{
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/hit_mhs',
-      'note':
+      'z_note':
       'this route for search of list mahasiswa data. By input a text. available to input by number of student, university, name, or the combination of number, university, and name. after enter an input, url will redirect to this url /hit_mhs/<string:mahasiswa>'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/hit_mhs/<string:mahasiswa>',
-      'note':
+      'z_note':
       'this route for show list result after input the text from route /hit_mhs. the output is a json data (text: name, nim, pt, prodi. website-link:url/id)'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/data_mahasiswa',
-      'note':
+      'z_note':
       'this route for search detail of mahasiswa using id. will redirect to /data_mahasiswa/<string:id_mahasiswa>'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/data_mahasiswa/<string:id_mahasiswa>',
-      'note':
+      'z_note':
       'this route for show detail of mahasiswa by id. the json data is datastatuskuliah : id_smt, nm_stat_mhs, sks_smt. datastudi : id_smt, kode_mk, nilai_huruf, nm_mk, sks_mk. dataumum : jk, ket_keluar, link_pt, link_prodi, mulai_smt, namajenjang, namaprodi, namapt, nipd, nm_jns_daftar, nm_pd, nm_prodi_asal, n_pt_asal, no_seri_ijazah, reg_pd, ret_prof, tgl_keluar.'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/load_pt',
-      'note':
+      'z_note':
       'this route for show list if pt. id_sp, kode_pt, nama_pt.'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/data_pt',
-      'note':
+      'z_note':
       'this route using for input data id of pt. and redirect to /data_pt/<string:link_pt>.'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/data_pt/<string:link_pt>',
-      'note':
+      'z_note':
       'this route using for detail of pt. json data such as akreditasi_list : akreditasi, tgl_akreditasi, tgl_berlaku. bujur, email, id_sp, internet, jln, kode_pos, laporatorium, lintang, listrik, luas_tanah, nama_rektor, nama_wil, nm_lemb, no_fax, no_tel, npsn, perpustakaan, ruang_kelas, sk_pendirian_sp, stat_sp, tgl_perdiri, tgl_sk_pendirian_sp, website.'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/data_pt_prodi',
-      'note':
+      'z_note':
       'this route for search detail prodi of pt by id of pt. will redirect to /data_pt_prodi/<string:link_pt>.'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/data_pt_prodi/<string:link_pt>',
-      'note':
+      'z_note':
       'this route for json data. such as akreditasi, id_sms, jenjang, kode_prodi, nm_lemb, rasio_list : dosen, dosenNidk, dosenNidn, mahasiswa, semester. stat_prodi.'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/data_pt_jumlah',
-      'note':
+      'z_note':
       'this route for search statistic from pt by id. will redirect to /data_pt_jumlah/<string:link_pt>'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/data_pt_jumlah/<string:link_pt>',
-      'note':
+      'z_note':
       'this route for json data of statistic by id. such as jumlah_bidangilmu, jumlah_fakultas, jumlah_prodi, jumlah_prodi_akreditasi : A, B, Baik sekali, C, unggul. rasio_list.'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/data_pt_dosen',
-      'note':
+      'z_note':
       'this route for search statistic of dosen from pt by id. will redirect to /data_pt_dosen/<string:link_pt>.'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/data_pt_dosen/<string:link_pt>',
-      'note':
+      'z_note':
       'this route for json data. such as tetap : jumlah_dosen_jabatan, Categories, series, data, name, jumlah_dosen_jenis_kelamin, L, P, jumlah_dosen_jenjang, jumlah_dosen_registrasi, tidak_tetap.'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/stat_pt',
-      'note':
+      'z_note':
       'this route for search statistic data of pt. will redirect to /stat_pt/<string:link_pt>.'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/stat_pt/<string:link_pt>',
-      'note':
+      'z_note':
       'this route for json data statistic of pt. such as id, rasio : nm_jenj_didik, total_kurang_6, total_lebih_, total_semua. rata_lama_studi : nm_jenj_didik, total_cont, total_years.'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/load_prodi',
-      'note':
+      'z_note':
       'this route for all list of prodi without filter. such as id_sms, id_sp, kode_prodi, nama_prodi.'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/load_prodi/<string:id_sp>',
-      'note':
+      'z_note':
       'this route for list of prodi using filter by id_sp. json data is id_sms, id_sp, kode_prodi, nama_prodi.'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/data_prodi/<string:link_prodi>',
-      'note':
+      'z_note':
       'this route for list json data of prodi data. such as datadosen : gelar, id, idreg, linkdosen, nama, pendidikan. datadosenrasio : gelar_dosen, id, idreg, jenjang_dosen, jenjang_komebase, linkdosen, nama, nidn, prodi_homebase, pt. datamhs : jml, mulai_smt. detailumum : akreditasi, bujur, capaian, deskripsi, email, id_sms, jln, kode_pos, kode_prodi, kompetensi, linkpt, lintang, misi, , namajenjang, namapt, nm_lemb, no_fax, no_tel, npsn, sk_selenggara, visi. rasio : jmldosen, jmlmhs, kode_program_studi, smt.'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/data_dosen/<string:link_dosen>',
-      'note':
+      'z_note':
       'this route for json data of dosen data. such as datamengajar : id_smt, kode_mk, linkpt, namapt, nm_kls, nm_mk. datapendidikan : namajenjang, nm_sp_formal, singkatan_gelar, thn_lulus. dataumum : foto, fungsional, -d_sdm, ikatankerja, jk, linkprodi, linkpt, namaprodi, namapt, nm_sdm, pend_tinggi, statuskeaktifan, tmpt_lahir'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/hit',
-      'note':
+      'z_note':
       'this route for search of list pt, dosen, prodi data. By input a text. available to input by all available of data search. after enter an input, url will redirect to this url /hit/<string:dsn_prodi_pt>'
-      },
-      {
+    }, {
       'url_base':
       'https://data-mahasiswa.ridwaanhall.repl.co',
       'route':
       '/hit/<string:dsn_prodi_pt>',
-      'note':
+      'z_note':
       'this route for json data detail of data dsn_prodi_pt. such as dosen : name, nidn, pt, prodi. prodi : , pt : pt, npsn, singkatan, alamat, link data'
-      }
-    ],
-    'source': 'pddikti'
+    }],
+    'source':
+    'pddikti'
   }
 
 
